682eda3d066e6adbd278b0de16cae51268d93e3e
Asad Aizaz <asad.aizaz99@gmail.com>
2016-08-31 15:04:17 -0700

    New Fragment to show upcoming weather

---
+++ app/src/main/java/com/asad/android/weathernowv2/WeatherDetailsFragment.java
@@ -0,0 +1,18 @@
+package com.asad.android.weathernowv2;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Asad on 8/31/2016.
+ */
+public class WeatherDetailsFragment extends Fragment{
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
